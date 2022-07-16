9e26544339b4971690659225f52ee6eaefec8d04
forrest liu <shp32131@atzy>
2022-07-16 15:09:44 +0800

    update config file

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -90,7 +90,7 @@ export default defineConfig({
 
     // the base URL the site will be deployed
     // base: '/',
-    base: '/frontend.github.io/',
+    base: '/frontend.github.io',
 
     /**
      * Use git commit to get the timestamp.
